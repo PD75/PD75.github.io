a2fc4c9d9c5d0d13318c025372d4d89a8587ae33
Paul <tigercub.75@gmail.com>
2016-03-28 17:42:27 +0200

    update gulp script

---
+++ app/ui-angular/ui-angular.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('UICtrl', UICtrl);
+
+  function UICtrl() {
+    var vm = this;
+    // vm.isActive = isActive;
+
+    vm.url = '#ui-angular';
+    vm.stickyData = {
+      offset: 60,
+    };
+
+   
+  }
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,27 @@
 
 // Include gulp
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
-var build = require('./distDev/ui-angular/tasks/build')(__dirname);
+var buildUIAngular = require('./distDev/ui-angular/tasks/build')(__dirname);
 
 
-gulp.task('build',build);
+gulp.task('build', ['installDist']);
+
+gulp.task('installDist', function() {
+  var paths  = [
+    './node_modules/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
+    './node_modules/angulartics/dist/angulartics.min.js',
+  ];
+  
+  
+  //  gulp.src()
+  //   .pipe(plugins.print())
+  //   .pipe(gulp.dest('./dist'));
+
+  return gulp.src(paths)
+    .pipe(plugins.print())
+    .pipe(gulp.dest('./dist'));
+
+
+});
