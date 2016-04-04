43407964bbd8bf56c8f5a25f751495a2c1098c28
Paul <tigercub.75@gmail.com>
2016-04-04 09:13:52 +0200

    changed some paths

--- app/index/index.js
+++ app/index/index.js
@@ -17,7 +17,8 @@
     function isActive(path) {
       return path === $location.path();
     }
-
+    
+    initGA();
     if ($location.host() === 'pd75.github.io') {
       ga('create', 'UA-75343768-1', 'auto');
 
@@ -41,5 +42,14 @@
         }
       });
     }
+  }
+
+  function initGA() {
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
   }
 })();
