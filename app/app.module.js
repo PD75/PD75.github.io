fe34860fa0d02f95dedff49196b96f57ff85960d
Paul Dhillon <tigercub.75@gmail.com>
2017-03-30 14:44:45 +0200

    added social buttons

--- app/app.module.js
+++ app/app.module.js
@@ -1,19 +1,19 @@
 (function() {
   'use strict';
-  angular.module('app', ['ngRoute', 'uiAngular', 'djds4rce.angular-socialshare', 'angulike', 'angulartics', 'angulartics.google.analytics']);
+  angular.module('app', ['ngRoute', 'uiAngular', 'djds4rce.angular-socialshare', 'angulike', 'angulartics', 'angulartics.google.analytics', '720kb.socialshare']);
 
 
   angular.module('app')
     .config(appConfig);
 
-  function appConfig($routeProvider) {
+  function appConfig(socialshareConfProvider, $routeProvider) {
     $routeProvider.
-      // when('/', {
-      //   templateUrl: 'app/home.html',
-      // }).
-      // when('/consulting', {
-      //   templateUrl: 'app/consulting.html',
-      // }).
+    // when('/', {
+    //   templateUrl: 'app/home.html',
+    // }).
+    // when('/consulting', {
+    //   templateUrl: 'app/consulting.html',
+    // }).
     when('/practical-startpage', {
       templateUrl: 'app/practical-startpage/practical-startpage.html',
       controller: 'PSCtrl',
@@ -32,5 +32,43 @@
     otherwise({
       redirectTo: '/practical-startpage',
     });
+    socialshareConfProvider.configure([{
+      'provider': 'facebook',
+      'conf': {
+        'url': 'https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+        'text': 'Practical Startpage ',
+      }
+    },{
+      'provider': 'xing',
+      'conf': {
+        'url': 'https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+        'text': 'Practical Startpage ',
+      }
+    },{
+      'provider': 'email',
+      'conf': {
+        'body': 'Hi, try Practical Startpage: https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+        'subject': 'Try Practical Startpage for Google chrome',
+      }
+    // },{
+    //   'provider': 'sms',
+    //   'conf': {
+    //     'url': 'https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+    //     'text': 'Practical Startpage ',
+    //   }
+    },{
+      'provider': 'facebook-messenger',
+      'conf': {
+        'url': 'https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+        'text': 'Practical Startpage ',
+      }
+    },{
+      'provider': 'whatsapp',
+      'conf': {
+        'url': 'https://chrome.google.com/webstore/detail/practical-startpage/ikjalccfdoghanieehppljppanjlmkcf',
+        'text': 'Practical Startpage: ',
+      }
+    }]);
+
   }
 })();
